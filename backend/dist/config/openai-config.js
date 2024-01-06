c83839316d91b17bca88ead3bde6fd193c8260ce
JerryZhou54 <69577934+JerryZhou54@users.noreply.github.com>
2024-01-06 10:04:56 -0500

    Delete backend/dist directory

--- backend/dist/config/openai-config.js
+++
@@ -1,8 +0,0 @@
-export const configureOpenAI = () => {
-    const config = {
-        apiKey: process.env.OPEN_AI_SECRET,
-        organization: process.env.OPEN_AI_ORG_ID,
-    };
-    return config;
-};
-//# sourceMappingURL=openai-config.js.map
--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRoutes from "./user-routes.js";
-import chatRoutes from "./chat-routes.js";
-const appRouter = Router();
-appRouter.use("/user", userRoutes); //domain/api/v1/user
-appRouter.use("/chats", chatRoutes); //domain/api/v1/chats
-export default appRouter;
-//# sourceMappingURL=index.js.map
