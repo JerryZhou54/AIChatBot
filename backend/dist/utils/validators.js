9b09bd470fb441725e6c8056794c375f033c970a
Zhou, Wei <wzhou322@gatech.edu>
2024-01-06 09:07:51 -0500

    Commit

---
+++ backend/dist/utils/validators.js
@@ -0,0 +1,35 @@
+import { body, validationResult } from "express-validator";
+export const validate = (validations) => {
+    return async (req, res, next) => {
+        for (let validation of validations) {
+            const result = await validation.run(req);
+            if (!result.isEmpty()) {
+                break;
+            }
+        }
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+        return res.status(422).json({ errors: errors.array() });
+    };
+};
+export const loginValidator = [
+    body("email").trim().isEmail().withMessage("Email is required"),
+    body("password")
+        .trim()
+        .isLength({ min: 6 })
+        .withMessage("Password should contain at least 6 characters")
+];
+export const signupValidator = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").trim().isEmail().withMessage("Email is required"),
+    body("password")
+        .trim()
+        .isLength({ min: 6 })
+        .withMessage("Password should contain at least 6 characters")
+];
+export const chatCompletionValidator = [
+    body("message").notEmpty().withMessage("Message is required"),
+];
+//# sourceMappingURL=validators.js.map
