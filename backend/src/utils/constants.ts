21ecb44c6943aa69cf003d97541c59c80035b17a
Zhou, Wei <wzhou322@gatech.edu>
2024-01-07 18:33:57 -0500

    Commit

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const COOKIE_NAME="auth_token";
 //export const BASE_DOMAIN = "https://frontend-jhtu.onrender.com";
 //export const BASE_DOMAIN = "localhost";
-export const BASE_DOMAIN = "https://aichatbot-bca0d.firebaseapp.com";
+export const BASE_DOMAIN = "https://aichatbot-bca0d.web.app";
