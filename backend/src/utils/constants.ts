ecdb4cc4588605afa8275e97b0d76ee1c2990dad
Zhou, Wei <wzhou322@gatech.edu>
2024-01-07 18:03:47 -0500

    Commit

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const COOKIE_NAME="auth_token";
-//export const BASE_DOMAIN = "https://frontend-jhtu.onrender.com";
-export const BASE_DOMAIN = "localhost";
+export const BASE_DOMAIN = "https://frontend-jhtu.onrender.com";
+//export const BASE_DOMAIN = "localhost";
