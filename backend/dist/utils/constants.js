1d322ab51c670c33fff4af3b1cfca4311cd501fc
Zhou, Wei <wzhou322@gatech.edu>
2024-01-07 18:29:02 -0500

    Commit

--- backend/dist/utils/constants.js
+++ backend/dist/utils/constants.js
@@ -1,4 +1,5 @@
 export const COOKIE_NAME = "auth_token";
-export const BASE_DOMAIN = "https://frontend-jhtu.onrender.com";
+//export const BASE_DOMAIN = "https://frontend-jhtu.onrender.com";
 //export const BASE_DOMAIN = "localhost";
+export const BASE_DOMAIN = "https://aichatbot-bca0d.firebaseapp.com";
 //# sourceMappingURL=constants.js.map
